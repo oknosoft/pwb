66df98fde701d5cbd3ce4ee5b816062cedcb4f4a
unpete <info@oknosoft.ru>
2024-11-02 19:36:45 +0300

    рестарт сеанса в браузере

--- src/metadata/common/index.js
+++ src/metadata/common/index.js
@@ -9,6 +9,7 @@ import {region_layer} from '../../components/Builder/ToolWnds/RegionLayer';
 import templates_nested from 'wb-forms/dist/CalcOrder/TemplatesNested/dhtmlx';
 import {event_src} from 'metadata-react/common/proxy';
 import {event_src_ram} from './event_src_ram';
+import reload from './reload';
 import qs from 'qs';
 
 export default function ($p) {
@@ -19,6 +20,7 @@ export default function ($p) {
   templates_nested($p);
   event_src($p);
   event_src_ram($p);
+  reload($p);
   $p.iface.scale_svg = scale_svg;
   $p.utils.scale_svg = scale_svg;
   $p.utils.prm = () => qs.parse(location.search.replace('?',''));
